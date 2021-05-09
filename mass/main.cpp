e1933b834ebb746f3444ec454871f553fa07e735
Hajin Chung <hajinchung1@gmail.com>
2021-05-09 13:14:15 +0900

    update: mass onclick new planet

--- mass/main.cpp
+++ mass/main.cpp
@@ -1,4 +1,5 @@
 #include <SFML/Graphics.hpp>
+#include <vector>
 #include <time.h>
 #include <stdlib.h>
 #include <stdio.h>
@@ -8,8 +9,9 @@
 #define SCREEN_WIDTH 900
 #define SCREEN_HEIGHT 600
 #define WINDOW_TITLE "massive bodies!"
+#define G_CON 10
+#define EDIST 10
 
-#define PLANET_N 2
 #define PLANET_R 10
 
 void main_loop();
@@ -23,14 +25,14 @@ sf::RenderWindow window(sf::VideoMode(SCREEN_WIDTH, SCREEN_HEIGHT), WINDOW_TITLE
 
 typedef struct {
     float mass;
-    int pinned;
+    bool pinned;
     sf::Vector2f s;
     sf::Vector2f v;
     sf::Vector2f a;
 } Planet;
 
-Planet planets[PLANET_N];
-Planet tmp_planets[PLANET_N];
+std::vector<Planet> planets, tmp_planets;
+int planet_n;
 
 int main() 
 {
@@ -62,7 +64,7 @@ void init()
 {
     // init
     srand(time(NULL));
-    for(int i=0 ; i<PLANET_N ; i++)
+    for(int i=0 ; i<planet_n ; i++)
     {
         planets[i].mass = 100000;
         planets[i].s.x = normalized_random(0, SCREEN_WIDTH);
@@ -75,11 +77,13 @@ void init()
     // planets[PLANET_N-1].mass = 700000;
     // planets[PLANET_N-1].pinned = 1;
 
-    memcpy(&tmp_planets, &planets, sizeof(Planet) * PLANET_N);
+    // memcpy(&tmp_planets, &planets, sizeof(Planet) * PLANET_N);
+    tmp_planets = planets; 
 }
 
 void update(sf::Time elapsed)
 {
+    printf("Update\n");
     // update
     //
     // 1) get sum of force from other planets
@@ -89,19 +93,18 @@ void update(sf::Time elapsed)
     // 5) commit update (tmp -> planets)
 
     sf::Vector2f f;
-    float angle;
     float tf;
     float dt = elapsed.asSeconds();
     float ds;
 
-    for(int i=0 ; i<PLANET_N ; i++)
+    for(int i=0 ; i<planet_n; i++)
     {
         f = {0, 0};
-        for(int j=0 ; j<PLANET_N ; j++)
+        for(int j=0 ; j<planet_n; j++)
             if(i!=j)
             {
                 ds = dist(planets[i].s, planets[j].s);
-                if(ds > 10)
+                if(ds > PLANET_R * 2)
                 {
                     tf = planets[i].mass * planets[j].mass / (ds * ds); 
                     f.y += tf * (planets[j].s.y - planets[i].s.y) / ds;
@@ -111,6 +114,7 @@ void update(sf::Time elapsed)
 
         if(planets[i].pinned)
             continue;
+
         tmp_planets[i].mass = planets[i].mass;
         tmp_planets[i].pinned = planets[i].pinned;
         tmp_planets[i].a = f / planets[i].mass;
@@ -118,16 +122,35 @@ void update(sf::Time elapsed)
         tmp_planets[i].s = planets[i].s + tmp_planets[i].v * dt;
     }
 
-    for(int i=0 ; i<PLANET_N ; i++) memcpy(&planets[i], &tmp_planets[i], sizeof(Planet));
+    planets = tmp_planets;
+
+    // on mouse click add new planet
+    if(sf::Mouse::isButtonPressed(sf::Mouse::Left))
+    {
+        planet_n++;
+        sf::Vector2i position = sf::Mouse::getPosition(window); 
+        Planet new_planet;
+        
+        new_planet.mass = 5000;
+        new_planet.pinned = false;
+        new_planet.a = new_planet.v = {0, 0};
+        new_planet.s = {(float)position.x, (float)position.y};
+
+        planets.push_back(new_planet);
+        tmp_planets = planets;
+        printf("%d %f %f mouse clicked! %d\n", planet_n, new_planet.s.x, new_planet.s.y, planet_n);
+    }
+
 }
 
 void render()
 {
+    printf("Render!\n");
     // render
 
     window.clear();
 
-    for(int i=0 ; i<PLANET_N ; i++)
+    for(int i=0 ; i<planet_n ; i++)
     {
         sf::CircleShape circle(PLANET_R);
         circle.setPosition(planets[i].s.x, planets[i].s.y);
